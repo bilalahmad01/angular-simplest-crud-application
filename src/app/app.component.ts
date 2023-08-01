a53ce0c9cf933fd074f0a10f4dee363f23457a29
Bilal Ahmad <bilal.ahmad66229@gmail.com>
2023-08-01 17:00:58 +0500

    AngularCRUD

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,48 @@
 import { Component } from '@angular/core';
-
+import {FormBuilder, Validators} from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'AngularCRUD';
+  constructor(public fb:FormBuilder){}
+  SubmitForm = this.fb.group({
+    Name: ['', Validators.compose([Validators.required])],
+    Email: ['', Validators.compose([Validators.required])],
+    Salary: ['', Validators.compose([Validators.required])]
+  })
+
+  Data:any=[];
+  saveItem:any;
+  OnSubmit(){
+    const getDataIndex = this.Data.findIndex((data: any) => data.Email === this.saveItem);
+    if (getDataIndex !== -1) {
+    this.Data[getDataIndex].Name = this.SubmitForm.value.Name;
+    this.Data[getDataIndex].Email = this.SubmitForm.value.Email;
+    this.Data[getDataIndex].Salary = this.SubmitForm.value.Salary;
+    }else{
+    this.Data.push(this.SubmitForm.value);
+    }
+    this.onReset();
+  }
+
+  onEdit(item:any){
+    this.saveItem = item;
+    const getData = this.Data.find((data:any) => data.Email==item);
+    this.SubmitForm.patchValue({
+      Name: getData.Name,
+      Email: getData.Email,
+      Salary: getData.Salary,
+    });
+  }
+
+  onDelete(item:any){
+    this.Data = this.Data.filter((data:any) => data.Email !== item);
+  }
+
+  onReset(){
+    this.SubmitForm.reset();
+    this.saveItem = "";
+  }
 }
